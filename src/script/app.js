aa3104d34d5e505021559b74a1984a7771683aa5
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-06-20 22:41:46 +0400

    I complete the task

--- src/script/app.js
+++ src/script/app.js
@@ -1,84 +1,38 @@
-document.addEventListener("DOMContentLoaded", runToAllEvents); // Target When page Loaded
-
-// ===> Js Selectors <===
-let selectors = {
-    form: document.querySelector("form"),
-    inputs: document.querySelectorAll("input"),
-    submitButton: document.querySelector("button"),
-    visible: document.querySelector(".fa-eye"),
-    passwordContainer: document.querySelector(".input-container"),
-};
-// ===> Destructing values <===
-const { form, inputs, submitButton, visible, passwordContainer } = selectors;
-const [firstname, email, password] = inputs;
-
-// ===> Output of all Events <===
-function runToAllEvents(e) {
-    if (form) {
-        form.addEventListener("submit", registerUser);
-        visible.addEventListener("click", switchType);
-    } else {
-        let msg = new Error("Error when page load...");
-        errorHandler(msg);
-    }
-}
-// ===> Custom Error Message <===
-const errorHandler = (msg) => {
-    console.error(msg);
-    window.location.reload();
-}
-// ===> Register Method <===
-function registerUser(e) {
-    const local_key = "users"
-    e.preventDefault();
-
-    // ===> Check the input fields <===
-    const fieldsFilled = Array.from(inputs).every((input, index) => {
-        if (input.value.trim() === "") {
-            if (index === 0) {
-                alert("Fullname must be filled");
-            } else if (index === 1) {
-                alert("Email must be filled");
-            } else if (index === 2) {
-                alert("Password must be filled");
+function getName() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let data = "Rahib";
+            if (data) {
+                resolve(data);
+            } else {
+                reject("Name is undefined")
             }
-            return false; // Return false to stop the iteration
-        }
-        return true; // Return true if the input is filled
+        }, 1000);
     });
-
-    if (!fieldsFilled) {
-        return;
-    };
-    // ===> Make an person object <===
-    let newUser = {
-        name: firstname.value.trim(),
-        email: email.value.trim(),
-        password: password.value.trim(),
-    }
-    // ===> check of the user already exist <===
-    let users = localStorage.getItem(local_key) ? JSON.parse(localStorage.getItem(local_key)) : [];
-    let existUsers = users.find((user) => user.email === newUser.email)
-
-
-    if (existUsers) {
-        alert("User with same email already exist");
-        return;
-    } else {
-        users.push(newUser);
-        alert(`User: ${newUser.name.substring(0, 1).toUpperCase() + newUser.name.slice(1)} created Successful`)
-        localStorage.setItem(local_key, JSON.stringify(users))
-        firstname.value = ""
-        email.value = ""
-        password.value = ""
-    }
-
 }
 
-// ===> Password input to text switcher <===
-function switchType(e) {
-    if (e.target) {
-        password.type = password.type === "password" ? "text" : password.type === "text" ? "password" : "text"
-    }
-
+function getSurname() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let data = "Rzayev";
+            if (data) {
+                resolve(data);
+            } else {
+                reject("Surname is undefined")
+            }
+        }, 2000);
+    });
 }
+
+Promise.all([getName(), getSurname()])
+    .then((res) => {
+        if (res) {
+            console.log(res[0]);
+            console.log(res[1]);
+        } else {
+            throw new Error("Error with data fetching ");
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
